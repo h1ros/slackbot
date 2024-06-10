92f7346b381ba422c135c87cdd5b1d33a561c3e4
Hiros <data.h1ros@gmail.com>
2024-06-09 23:11:42 -0700

    update requirements.txt

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from slack_bolt.async_app import AsyncApp
 from slack_bolt.adapter.fastapi.async_handler import AsyncSlackRequestHandler
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from mangum import Mangum
 import asyncio
 from config import SLACK_BOT_TOKEN, SLACK_SIGNING_SECRET
@@ -14,8 +14,17 @@ api = FastAPI()
 # Slack request handler
 slack_handler = AsyncSlackRequestHandler(app)
 
+# @api.post("/slack/messages")
+# async def slack_events(request: Request):
+#     return {'status': 200}
+#     # return await slack_handler.handle(request)
 @api.post("/slack/messages")
-async def slack_events(request: fastapi.Request):
+async def slack_events(request: Request):
+    # logger.info('call /slack/messages')
+    print('call /slack/messages')
+    body = await request.json()
+    if "challenge" in body:
+        return {"challenge": body["challenge"]}
     return await slack_handler.handle(request)
 
 # Define lazy listener for handling messages
@@ -42,4 +51,5 @@ async def process_mention(event, say):
     await say(f"Hello <@{event['user']}>! How can I help you?")
 
 # Lambda handler
+print('Starting up')
 handler = Mangum(api)
